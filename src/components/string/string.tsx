4b4bfc0d9f485cf585873ab4e04304dd73d73de7
Igor Topal <mr.topal89@gmail.com>
2022-11-14 23:26:55 +0300

    feat/add-string-visualization

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,10 +1,78 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
+import { ElementStates } from "../../types/element-states";
+import { delay } from "../../utils/delay";
+import { swap } from "../../utils/swap";
+import { Button } from "../ui/button/button";
+import { Circle } from "../ui/circle/circle";
+import { Input } from "../ui/input/input";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import styles from "./string.module.css";
 
 export const StringComponent: React.FC = () => {
+  const [inputValue, setInputValue] = useState<string>("");
+  const [lettersValue, setLettersValue] = useState<
+    (string | number | ElementStates)[][]
+  >([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const onInputChange = useCallback((e) => {
+    setInputValue(e.target.value);
+  }, []);
+
+  const onButtonClick = useCallback(async () => {
+    setIsLoading(true);
+    let arr: (string | number | ElementStates)[][] = inputValue
+      .split("")
+      .map((e, index) => [e, index]);
+    let end = arr.length - 1;
+    for (let start = 0; start <= end; start++) {
+      if (start === end) {
+        arr[start].push(ElementStates.Modified);
+        setLettersValue([...arr]);
+        break;
+      }
+      arr[start].push(ElementStates.Changing);
+      arr[end].push(ElementStates.Changing);
+      setLettersValue([...arr]);
+      swap(arr, start, end);
+      await delay(1000);
+      setLettersValue([...arr]);
+      arr[start].pop();
+      arr[end].pop();
+      arr[start].push(ElementStates.Modified);
+      arr[end].push(ElementStates.Modified);
+      end--;
+    }
+    setLettersValue([...arr]);
+    setIsLoading(false);
+  }, [inputValue]);
+
   return (
     <SolutionLayout title="Строка">
-     
+      <div className={styles.inputContainer}>
+        <Input
+          isLimitText={true}
+          maxLength={11}
+          value={inputValue}
+          onChange={onInputChange}
+          disabled={isLoading}
+        />
+        <Button
+          text="Развернуть"
+          disabled={!inputValue.length}
+          isLoader={isLoading}
+          onClick={onButtonClick}
+        />
+      </div>
+      <div className={styles.lettersContainer}>
+        {lettersValue.map((e) => (
+          <Circle
+            letter={e[0] as string}
+            key={e[1]}
+            state={e[2] as ElementStates}
+          />
+        ))}
+      </div>
     </SolutionLayout>
   );
 };
---
+++ src/utils/swap.ts
@@ -0,0 +1,5 @@
+export const swap = (arr: any[], firstIndex: number, secondIndex: number) => {
+  const temp = arr[firstIndex];
+  arr[firstIndex] = arr[secondIndex];
+  arr[secondIndex] = temp;
+}
